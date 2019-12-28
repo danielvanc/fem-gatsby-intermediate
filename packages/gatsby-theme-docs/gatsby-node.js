f3051d1664b2a3993f29c76343c3857a62090b2a
danielvanc <email@danielvanc.com>
2019-12-28 13:58:09 +0000

    feat(theme-dev): add schema customization

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -17,3 +17,63 @@ exports.onPreBootstrap = ({ store }, options) => {
     mkdirp.sync(dir)
   }
 }
+
+exports.createSchemaCustomization = ({ actions }) => {
+  actions.createTypes(`
+    type DocsPage implements Node @dontInfer {
+      id: ID!
+      title: String!
+      path: String!
+      updated: Date! @dateformat
+      body: String!
+    }
+  `)
+}
+// node = node that was created, the actions we can do with the node, load the node, helper to create node id
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+  const { basePath } = withDefaults(options);
+  const parent = getNode(node.parent);
+
+  // only work on mdx files that were loaded by this theme
+  if (
+    node.internal.type !== 'Mdx' || parent.sourceInstanceName !== 'gatsby-theme-docs'
+  ) {
+    return;
+  }
+  // Treat 'index.mdx' link `index.html` (.ie `docs/ vs docs/index)
+  const pageName = parent.name !== 'index' ? parent.name : '';
+
+  actions.createNode({
+    id: createNodeId(`DocsPage-${node.id}`),
+    title: node.frontmatter.title || parent.name,
+    updated: parent.modifiedTime,
+    path: path.join('/', basePath, parent.relativeDirectory, pageName),
+    parent: node.id,
+    internal: {
+      type: 'DocsPage',
+      contentDigest: node.internal.contentDigest,
+    }
+  })
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+  createResolvers({
+    DocsPage: {
+      body: {
+        type: 'String!',
+        resolve: (source, args, context, info) => {
+          // Load the resolver for the Mdx type: body field
+          const type = info.schema.getType('Mdx');
+          const mdxFields = type.getFields();
+          const resolver = mdxFields.body.resolve;
+
+          const mdxNode = context.nodeModel.getNodeById({ id: source.parent })
+
+          return resolver(mdxNode, args, context, {
+            fieldName: 'body'
+          })
+        }
+      }
+    }
+  })
+}
