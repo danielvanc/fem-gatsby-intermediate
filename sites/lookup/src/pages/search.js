af240f1f9536238fd9d155ede0e0911a7aaee19d
danielvanc <email@danielvanc.com>
2020-01-06 19:33:19 +0000

    add results

--- sites/lookup/src/pages/search.js
+++ sites/lookup/src/pages/search.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import Form from '../components/form'
+import Form from '../components/form';
+import Results from '../components/results';
 
-const Index = () => (
-  <>
-    <h1>Search for Rick and Morty Characters</h1>
-    <p>
-      Trying to remember which Rick you're talking about? Try out this handy search interface.
-    </p>
-    
-    <Form />
-    {/* TODO Add results  */}
-  </>
-)
+const Index = ({ location }) => {
+  const query = (location.state && location.state.query || location.pathname.replace(/^\/search\/?/, '') || '');
+
+  const name = query.replace(/-+/g, ' ');
+
+
+
+  return (
+    <>
+      <h1>{ name ? `Showing results for "${name}"` : 'Search for Rick and Morty Characters' }</h1>
+      <p>
+        Trying to remember which Rick you're talking about? Try out this handy search interface.
+      </p>
+      
+      <Form />
+      {name && <Results name={name} />}
+      {/* TODO Add results  */}
+    </>
+  )
+}
 
 export default Index;
